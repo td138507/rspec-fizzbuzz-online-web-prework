16073e196f2b248cd5bf3322e6cd2e355659f431
Taylor Doyle <tcd56@cornell.edu>
2019-04-09 03:26:23 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,16 @@
-require_relative './spec_helper.rb'
-# We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
-
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
-
-    expect(fizz_3).to eq("Fizz")
+def fizz_buzz(limit)
+  fizz_buzz_series = Array.new
+  number = 1
+  limit.times do
+    if (number % 3 == 0 && number % 5 == 0)
+      fizz_buzz_series.push('FizzBuzz')
+    elsif number % 3 == 0
+     fizz_buzz_series.push('Fizz')
+    elsif number % 5 == 0
+      fizz_buzz_series.push('Buzz')
+    else fizz_buzz_series.push(number)
+    end
+    number = number + 1
   end
-  it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = fizzbuzz(5)
-
-    expect(fizz_5).to eq("Buzz")
-  end
-  it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
-    fizz_15 = fizzbuzz(15)
-
-    expect(fizz_15).to eq("FizzBuzz")
-  end
-  it 'returns nil when the number is not divisible by 3 or 5' do
-    fizz_4 = fizzbuzz(4)
-
-    expect(fizz_4).to eq(nil)
-  end
+  fizz_buzz_series
 end
